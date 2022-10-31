52ee0e37559ef72f83b5de17dcc6759d55a91d85
Kayleigh Watts <kwatts949@gmail.com>
2022-10-31 13:41:28 +0000

    Add sort method

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -1,12 +1,31 @@
 require 'sinatra/base'
 
 class Application < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
 
   get '/hello' do
      name = params[:name]
      return "Hello #{name}"
   end
+
+  get '/names' do
+     return "Julia, Mary, Karim"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    return "Thanks #{name} you sent this message: #{message}"
+  end
+
+  post '/sort-names' do
+    names = params[:names]
+    return names.split(",").sort.join(",")
+  end
 end
+
+# Postman request
+# GET
+# http://localhost:9292/hello?name=Maria
+
+# POST
+# http://localhost:9292/submit?name=Kay&message='Hello World'
